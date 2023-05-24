7d05904ae263f7284f71703785cb4801b1d3e083
Anton <41597155+antontidev@users.noreply.github.com>
2023-05-24 16:13:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var cors = require('cors');
 
 app.use(cors())
 app.use(express.static(__dirname + '/public')); 
+app.use(function (req, res, next) {
+  req.headers['content-type'] = 'br';
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port);
