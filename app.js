09932f37cbf4ecea757f1084efb9449d09c49599
Anton <41597155+antontidev@users.noreply.github.com>
2023-05-30 01:12:20 +0500

    Change port

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var cors = require('cors');
 app.use(cors())
 app.use(express.static(__dirname + '/public'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port);
