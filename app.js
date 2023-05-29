ef08021cc9d20ece56f0bbf6ddebae72288e9b03
Anton Sukharev <sanjaplushka@gmail.com>
2023-05-29 23:04:19 +0500

    Remove cors

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ var app = express();
 var cors = require('cors');
 
 app.use(cors())
-app.use(express.static(__dirname + '/public')); 
-app.use(function (req, res, next) {
-  res.set('Bypass-Tunnel-Reminder', 'text/plain');
-  req.headers['content-type'] = 'br';
-});
+app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 3000;
 app.listen(port);
