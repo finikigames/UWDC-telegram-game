342393f9eb3a52b46e85a74f47cb7fe13df3444e
Anton Sukharev <sanjaplushka@gmail.com>
2023-05-30 14:11:22 +0500

    Add port

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var cors = require('cors');
 app.use(cors())
 app.use(express.static(__dirname + '/public'));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 80;
 app.listen(port);
