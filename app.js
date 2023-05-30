5117b8b24858cdff64fb242f86662f6deb3f11b6
Anton Sukharev <sanjaplushka@gmail.com>
2023-05-30 11:38:25 +0500

    Add docker-compose file

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 var express = require('express');
-var https = require("https");
-var http = require('http');
-var fs = require("fs");
+var app = express();
 var cors = require('cors');
 
-var app = express();
-
-http.createServer(app).listen(80);
-
-var app = express();
-
 app.use(cors())
-
-https
-  .createServer(
-		// Provide the private and public key to the server by reading each
-		// file's content with the readFileSync() method.
-    {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
-    },
-    app
-  )
-  .listen(443, () => {
-    console.log("serever is runing at port 80");
-  });
-
 app.use(express.static(__dirname + '/public'));
+
+const port = process.env.PORT || 80;
+app.listen(port);
