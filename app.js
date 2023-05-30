0909ef521108e1ae4c9496b737d759f0d341d6c7
Anton Sukharev <sanjaplushka@gmail.com>
2023-05-30 09:51:39 +0500

    Add https

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require('express');
-var app = express();
+var https = require("https");
+var fs = request("fs");
 var cors = require('cors');
+
+var app = express();
 
 app.use(cors())
 app.use(express.static(__dirname + '/public'));
 
-const port = process.env.PORT || 80;
-app.listen(port);
+https
+  .createServer(
+		// Provide the private and public key to the server by reading each
+		// file's content with the readFileSync() method.
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(80, () => {
+    console.log("serever is runing at port 80");
+  });
