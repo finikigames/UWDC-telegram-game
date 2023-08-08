a81b75a2cc8a7519345d9be8879922f8b9726478
Anton Sukharev <sanjaplushka@gmail.com>
2023-08-08 10:21:03 +0500

    Add develop directory

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var app = express();
 var cors = require('cors');
 
 app.use(cors())
-app.use(express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + '/public'));
+app.use("/dev", express.static(__dirname + '/develop'));
 
 const port = process.env.PORT || 3000;
 app.listen(port);
