46b0cb0e6a3ab06df28e7bee32207f2d7917530f
Anton Sukharev <sanjaplushka@gmail.com>
2023-08-08 13:13:23 +0500

    Remove develop folder

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = express();
 var cors = require('cors');
 
 app.use(cors())
-app.use("/", express.static(__dirname + '/public'));
-app.use("/dev", express.static(__dirname + '/develop'));
+app.use(express.static(__dirname + '/public'));
+//app.use("/dev", express.static(__dirname + '/develop'));
 
 const port = process.env.PORT || 3000;
 app.listen(port);
